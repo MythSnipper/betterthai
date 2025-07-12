bb836f8cabcc60ba84364ac8d00d02f417d9caf4
MythSnipper <mythsnipper2022@gmail.com>
2025-07-12 22:16:29 +0300

    made more complex thing with char buffer

--- src/betterthai.el
+++ src/betterthai.el
@@ -1,10 +1,14 @@
 (defvar betterthai-ime-mode nil
   "Non-nil if BetterThai IME mode is enabled.")
 
+(defvar betterthai-ime-char-buffer ""
+  "Buffer that accumulates characters typed in IME mode.")
+
 (defun betterthai-toggle-ime ()
   "Toggle BetterThai IME mode."
   (interactive)
   (setq betterthai-ime-mode (not betterthai-ime-mode))
+  (setq betterthai-ime-char-buffer "") ; clear buffer on toggle
   (message "BetterThai IME %s" (if betterthai-ime-mode "ON" "OFF")))
 
 (defvar betterthai-ime-map
@@ -15,8 +19,20 @@
         (lambda ()
           (interactive)
           (if betterthai-ime-mode
-              (message "Character pressed")
+              (progn
+                (setq betterthai-ime-char-buffer
+                      (concat betterthai-ime-char-buffer (string last-command-event)))
+                (message "Buffer: %s" betterthai-ime-char-buffer))
             (self-insert-command 1)))))
+    ;; Bind Backspace in IME mode to clear buffer
+    (define-key map (kbd "DEL")
+      (lambda ()
+        (interactive)
+        (if betterthai-ime-mode
+            (progn
+              (setq betterthai-ime-char-buffer "")
+              (message "Buffer cleared"))
+          (backward-delete-char-untabify 1))))
     map)
   "Keymap for BetterThai IME minor mode.")
 
